6ff53ddb09ee9ce5bd3b156d2b8d7077bb3bde15
Harrison Malone <harrison.malone2@gmail.com>
2020-05-15 12:39:01 +1000

    added the center value in the json response, also added search action

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,14 +5,23 @@ class VenuesController < ApplicationController
       data = @venues.map do |venue|
         [venue.latitude, venue.longitude]
       end 
-      render json: {data: data}
+      render json: {data: data, center: [data[0][0], data[0][1]]}
     end
   end 
 
   def show
     @venue = Venue.find(params[:id])
     if params[:type] == "json"
-      render json: {data: [@venue.latitude, @venue.longitude]}
+      render json: {data: [@venue.latitude, @venue.longitude], center: [@venue.latitude, @venue.longitude]}
     end
   end
+
+  def search
+    location = Geocoder.search(params[:search])[0].data["geometry"]["location"]
+    @venues = Venue.all
+    data = @venues.map do |venue|
+      [venue.latitude, venue.longitude]
+    end
+    render json: {data: data, center: [location["lat"], location["lng"]]}  
+  end 
 end
