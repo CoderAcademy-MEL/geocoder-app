710d6a0c4e73ea693dee83205a0b53b430437dc7
Harrison Malone <harrison.malone2@gmail.com>
2020-05-15 12:37:42 +1000

    routes file now has a search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/venues', to: "venues#index"
-  get 'venues/:id', to: "venues#show"
+  get '/venues/:id', to: "venues#show"
+  get '/venues/map/:search', to: "venues#search"
 end
