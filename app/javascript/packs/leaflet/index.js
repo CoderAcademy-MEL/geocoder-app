2cade5b45a5a5eb272edeff5a05a40af2e81c1a0
Harrison Malone <harrison.malone2@gmail.com>
2020-05-15 12:41:14 +1000

    changed index.js to have an event listener for a search form, also changed the way the map centers based on json response

--- app/javascript/packs/leaflet/index.js
+++ app/javascript/packs/leaflet/index.js
@@ -1,14 +1,24 @@
 import { icon } from './icon'
 import { tileLayer } from './tileLayer'
 
-const renderMap = async () => {
-  const response = await fetch(`/venues?type=json`)
-  const { data } = await response.json()
-  const map = await tileLayer(data)
+const renderMap = async (search) => {
+  const url = search ? `/venues/map/${search}` : `/venues?type=json`
+  const response = await fetch(url)
+  const { data, center } = await response.json()
+  const map = await tileLayer(center)
   const markers = data.map((location) => {
     return L.marker([location[0], location[1]], {icon: icon})
   })
   L.featureGroup(markers).addTo(map)
 }
+
+const search = document.querySelector("#search-form")
+search.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  map.remove()
+  document.querySelector(".map-container").innerHTML = `<div id="map"></div>`
+  const value = e.target.elements[0].value
+  renderMap(value)
+})
 
 renderMap()
--- app/javascript/packs/leaflet/tileLayer.js
+++ app/javascript/packs/leaflet/tileLayer.js
@@ -1,7 +1,8 @@
 import L from 'leaflet'
 
-export const tileLayer = async (data) => {
-  const view = typeof(data[0]) === "object" ? {zoom: 11, lat: -37.81, long: 144.96} : {zoom: 20, lat: data[0], long: data[1]} 
+export const tileLayer = async (center) => {
+  const view = {zoom: 11, lat: center[0], long: center[1]}
+  console.log(view)
   const map = L.map('map').setView([view.lat, view.long], view.zoom)
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
