81757dd33b57918b9aa735af98802d9ad2b88d56
Harrison Malone <harrison.malone2@gmail.com>
2020-05-19 12:46:54 +1000

    edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.serve_static_assets = true
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
